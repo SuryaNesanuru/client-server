64e86fc1b4f70d95f15e22f6ae2d78428cee5a80
SuryaNesanuru <nesanurusuryavaraprasad@gmail.com>
2024-09-01 23:32:09 +0530

    client and server components

---
+++ app/components/ExampleClient.tsx
@@ -0,0 +1,12 @@
+'use-client'
+
+const ExampleClient = () => {
+  console.log("I am a ExampleClient component");
+  return (
+    <div>
+      <p>This is a Client Component</p>
+    </div>
+  );
+}
+
+export default ExampleClient;
---
+++ app/components/ExampleServer.tsx
@@ -0,0 +1,10 @@
+const ExampleServer = () => {
+  console.log("I am a ExampleClient component");
+  return (
+    <div>
+      <p>This is a ServerComponent</p>
+    </div>
+  );
+};
+
+export default ExampleServer;
